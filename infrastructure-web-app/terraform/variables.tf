0a142035aa4485e924243032dec3bcd1d9d956f5
Adebayo <wakama0007@gmail.com>
2025-12-12 21:20:08 -0500

    chore: updated variables

--- infrastructure-web-app/terraform/variables.tf
+++ infrastructure-web-app/terraform/variables.tf
@@ -2,12 +2,12 @@
 
 variable "region" {
   type    = string
-  default = "us-east-1"
+  sensitive = true
 }
 
 variable "vpc_cidr" {
   type    = string
-  default = "10.0.0.0/16"
+  sensitive = true
 }
 
 variable "vpc_name" {
